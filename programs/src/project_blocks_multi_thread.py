e9e61fdcaea30b2c275bc67c7b6fa1f757c37f48
Mobin Asri <masri@ucsc.edu>
2023-03-31 06:24:11 +0000

    added --divergence option to the projecting script

--- programs/src/project_blocks_multi_thread.py
+++ programs/src/project_blocks_multi_thread.py
@@ -73,6 +73,8 @@ def main():
                     help='(BED format) A path for saving the projections of the query blocks.Note that the lines may not be sorted and may have overlaps because of its correspondence with the projected bed file. The 4-th column contains the disimilarity percentage = (indels + mismatches)/(projection block length) * 100. The 5-th column contains other info if present in the input bed file. It is recommended to run bedtools sort (and merge) on this output')
     parser.add_argument('--threads', type=int,
                     help='Number of threads')
+    parser.add_argument('--divergence',
+                    help='Print divergence percentage (between asm and ref block) as the 4th column in the output bed file')
     
     # Fetch the arguments
     args = parser.parse_args()
@@ -82,18 +84,23 @@ def main():
     outputProjectable = args.outputProjectable
     outputProjection = args.outputProjection
     threads = args.threads
+    printDiv = args.divergence
 
+    # Save the track line if there is one
+    trackLine = None
     # Read the desired blocks. Their start and end positions are converted into the 1-based format
     blocks = defaultdict(list)
     with open(blocksPath,"r") as f:
         for line in f:
-            if line.startswith("track name"): continue
+            if line.startswith("track name"):
+                trackLine = line
+                continue
             attrbs = line.strip().split()
             contigName = attrbs[0]
             # start is 0-based in bed format, it gets converted to 1-based here
             start = int(attrbs[1]) + 1
             end = int(attrbs[2])
-            info = attrbs[3] if len(attrbs) > 3 else ""
+            info = attrbs[3:] if len(attrbs) > 3 else [""]
             blocks[contigName].append((start, end, info))
 
     results = runProjectionParallel(pafPath, mode, blocks, threads)
@@ -105,15 +112,31 @@ def main():
             contigName = res[1]
             qBlocks = res[2]
             rBlocks = res[3]
+            if trackLine != None: # write track line if there was any
+                fRef.write(f"{trackLine}\n")
+                fQuery.write(f"{trackLine}\n")
+
             if mode == "asm2ref":
-                for rBlock in rBlocks:
-                    fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, rBlock[0] - 1, rBlock[1], rBlock[3], rBlock[2]))
-                for qBlock in qBlocks:
-                    fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, qBlock[0] - 1, qBlock[1], qBlock[3], qBlock[2]))
+                if printDiv == True: # print divergence percentage in the 4th column
+                    for rBlock in rBlocks:
+                        fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, rBlock[0] - 1, rBlock[1], rBlock[3], rBlock[2]))
+                    for qBlock in qBlocks:
+                        fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, qBlock[0] - 1, qBlock[1], qBlock[3], qBlock[2]))
+                else:  # skip divergence ratio
+                    for rBlock in rBlocks:
+                        fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, rBlock[0] - 1, rBlock[1], "\t".join(rBlock[2])))
+                    for qBlock in qBlocks:
+                        fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, qBlock[0] - 1, qBlock[1], "\t".join(qBlock[2])))
             else: # mode = "ref2asm"
-                for rBlock in rBlocks:
-                    fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, rBlock[0] - 1, rBlock[1], rBlock[3], rBlock[2]))
-                for qBlock in qBlocks:
-                    fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, qBlock[0] - 1, qBlock[1], rBlock[3], qBlock[2]))
+                if printDiv == True: # print divergence percentage in the 4th column
+                    for rBlock in rBlocks:
+                        fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, rBlock[0] - 1, rBlock[1], rBlock[3], rBlock[2]))
+                    for qBlock in qBlocks:
+                        fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, qBlock[0] - 1, qBlock[1], rBlock[3], qBlock[2]))
+                else: # skip divergence ratio
+                    for rBlock in rBlocks:
+                        fRef.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(contigName, rBlock[0] - 1, rBlock[1], "\t".join(rBlock[2])))
+                    for qBlock in qBlocks:
+                        fQuery.write("{}\t{}\t{}\t{:.3f}\t{}\n".format(chromName, qBlock[0] - 1, qBlock[1], "\t".join(qBlock[2])))
 main()
 
