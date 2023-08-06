6b915337ddbcce0e815be8d733ff7783e17e6c0f
mobinasri <masri@ucsc.edu>
2023-08-06 04:07:06 +0000

    Added some unit tests for projection

--- programs/src/block_utils_test.py
+++ programs/src/block_utils_test.py
@@ -8,7 +8,10 @@ class TestProjection(unittest.TestCase):
         """Define two alignments one in positive orientation and one in negative"""
         self.alignmentPositive = Alignment("ctg\t350\t100\t150\t+\tref\t158\t10\t58\t27\t48\t60\tcg:Z:4=1X2I2=1X10D1X2=10I1X1=5D5=1X4=5I3=1X6=\ttp:A:P")
         self.alignmentNegative = Alignment("ctg\t350\t200\t250\t-\tref\t158\t10\t58\t27\t48\t60\tcg:Z:4=1X2I2=1X10D1X2=10I1X1=5D5=1X4=5I3=1X6=\ttp:A:P")
+        print(f"Tests:")
 
+    #def setUp(self):
+    #    print(f"\t- {self._testMethodName}")
 
     def testPositiveAsm2Ref(self):
         alignment = self.alignmentPositive
@@ -114,16 +117,16 @@ class TestProjection(unittest.TestCase):
             self.assertEqual(rBlocks[i][1], projectionBlocks[i][1], "Incorrect projection end position")
             self.assertEqual(cigarListTruth[i], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
 
-    def testPositiveRef2AsmInclude(self):
+    def testPositiveRef2AsmIncludeEndingAndPostIndel(self):
         alignment = self.alignmentPositive
         mode = "ref2asm"
-        includeEndingIndel = False
-        includePostIndel = False
+        includeEndingIndel = True
+        includePostIndel = True
         # block start and end are 1-based
-        blocks = [(11, 16, "NA"), (29, 31, "NA"), (32, 35, "NA")]
-        projectionBlocks = [(101, 108), (111, 113), (124,125)]
-        projectableBlocks = [(11, 16), (29, 31), (32, 33)]
-        cigarListTruth = ["4=1X2I1=", "1X2=", "1X1="]
+        blocks = [(11, 20, "NA"), (21, 31, "NA"), (32, 35, "NA")]
+        projectionBlocks = [(101, 110), (111, 123), (124,125)]
+        projectableBlocks = [(11, 20), (21, 31), (32, 35)]
+        cigarListTruth = ["4=1X2I2=1X2D", "8D1X2=10I", "1X1=2D"]
         qBlocks, rBlocks, cigarList = findProjections(mode,
                                                       alignment.cigarList,
                                                       blocks,
@@ -140,4 +143,145 @@ class TestProjection(unittest.TestCase):
             self.assertEqual(rBlocks[i][1], projectionBlocks[i][1], "Incorrect projection end position")
             self.assertEqual(cigarListTruth[i], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
 
-unittest.main()
+    def testPositiveRef2AsmIncludeEndingIndel(self):
+        alignment = self.alignmentPositive
+        mode = "ref2asm"
+        includeEndingIndel = True
+        includePostIndel = True
+        # block start and end are 1-based
+        blocks = [(11, 20, "NA"), (21, 31, "NA"), (32, 48, "NA")]
+        projectionBlocks = [(101, 110), (111, 123), (124,140)]
+        projectableBlocks = [(11, 20), (21, 31), (32, 48)]
+        cigarListTruth = ["4=1X2I2=1X2D", "8D1X2=10I", "1X1=5D5=1X4=5I"]
+        qBlocks, rBlocks, cigarList = findProjections(mode,
+                                                      alignment.cigarList,
+                                                      blocks,
+                                                      alignment.chromLength,
+                                                      alignment.chromStart + 1, alignment.chromEnd, # make 1-based start
+                                                      alignment.contigLength,
+                                                      alignment.contigStart + 1, alignment.contigEnd, # make 1-based start
+                                                      alignment.orientation,
+                                                      includeEndingIndel, includePostIndel)
+        for i in range(3):
+            self.assertEqual(qBlocks[i][0], projectableBlocks[i][0], "Incorrect projectable start position")
+            self.assertEqual(qBlocks[i][1], projectableBlocks[i][1], "Incorrect projectable end position")
+            self.assertEqual(rBlocks[i][0], projectionBlocks[i][0], "Incorrect projection start position")
+            self.assertEqual(rBlocks[i][1], projectionBlocks[i][1], "Incorrect projection end position")
+            self.assertEqual(cigarListTruth[i], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
+
+
+    def testNegativeAsm2Ref(self):
+        alignment = self.alignmentNegative
+        mode = "asm2ref"
+        includeEndingIndel = False
+        includePostIndel = False
+        # block start and end are 1-based
+        blocks = [(150, 211, "NA"), (212, 225, "NA"), (240, 241, "NA")]
+        projectionBlocks = [(49, 58), (39, 48), (18, 29)]
+        projectableBlocks = [(201, 210), (216, 225), (240, 241)]
+        cigarListTruth = ["3=1X6=", "5=1X4=", "1X10D1X"]
+        qBlocks, rBlocks, cigarList = findProjections(mode,
+                                                      alignment.cigarList,
+                                                      blocks,
+                                                      alignment.chromLength,
+                                                      alignment.chromStart + 1, alignment.chromEnd, # make 1-based start
+                                                      alignment.contigLength,
+                                                      alignment.contigStart + 1, alignment.contigEnd, # make 1-based start
+                                                      alignment.orientation,
+                                                      includeEndingIndel, includePostIndel)
+        for i in range(3):
+            j = -1-i
+            self.assertEqual(qBlocks[j][0], projectableBlocks[i][0], "Incorrect projectable start position")
+            self.assertEqual(qBlocks[j][1], projectableBlocks[i][1], "Incorrect projectable end position")
+            self.assertEqual(rBlocks[j][0], projectionBlocks[i][0], "Incorrect projection start position")
+            self.assertEqual(rBlocks[j][1], projectionBlocks[i][1], "Incorrect projection end position")
+            self.assertEqual(cigarListTruth[j], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
+
+
+    def testNegativeAsm2RefIncludeEndingIndel(self):
+        alignment = self.alignmentNegative
+        mode = "asm2ref"
+        includeEndingIndel = True
+        includePostIndel = False
+        # block start and end are 1-based
+        blocks = [(150, 211, "NA"), (212, 225, "NA"), (240, 241, "NA")]
+        projectionBlocks = [(49, 58), (39, 48), (18, 29)]
+        projectableBlocks = [(201, 211), (212, 225), (240, 241)]
+        cigarListTruth = ["1I3=1X6=", "5=1X4=4I", "1X10D1X"]
+        qBlocks, rBlocks, cigarList = findProjections(mode,
+                                                      alignment.cigarList,
+                                                      blocks,
+                                                      alignment.chromLength,
+                                                      alignment.chromStart + 1, alignment.chromEnd, # make 1-based start
+                                                      alignment.contigLength,
+                                                      alignment.contigStart + 1, alignment.contigEnd, # make 1-based start
+                                                      alignment.orientation,
+                                                      includeEndingIndel, includePostIndel)
+        for i in range(3):
+            j = -1-i
+            self.assertEqual(qBlocks[j][0], projectableBlocks[i][0], "Incorrect projectable start position")
+            self.assertEqual(qBlocks[j][1], projectableBlocks[i][1], "Incorrect projectable end position")
+            self.assertEqual(rBlocks[j][0], projectionBlocks[i][0], "Incorrect projection start position")
+            self.assertEqual(rBlocks[j][1], projectionBlocks[i][1], "Incorrect projection end position")
+            self.assertEqual(cigarListTruth[j], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
+
+    def testNegativeAsm2RefIncludeEndingAndPostIndel(self):
+        alignment = self.alignmentNegative
+        mode = "asm2ref"
+        includeEndingIndel = True
+        includePostIndel = True
+        # block start and end are 1-based
+        blocks = [(150, 211, "NA"), (226, 227, "NA"), (240, 241, "NA")]
+        projectionBlocks = [(49, 58), (32, 38), (18, 29)]
+        projectableBlocks = [(201, 211), (226, 227), (240, 241)]
+        cigarListTruth = ["1I3=1X6=", "1X1=5D", "1X10D1X"]
+        qBlocks, rBlocks, cigarList = findProjections(mode,
+                                                      alignment.cigarList,
+                                                      blocks,
+                                                      alignment.chromLength,
+                                                      alignment.chromStart + 1, alignment.chromEnd, # make 1-based start
+                                                      alignment.contigLength,
+                                                      alignment.contigStart + 1, alignment.contigEnd, # make 1-based start
+                                                      alignment.orientation,
+                                                      includeEndingIndel, includePostIndel)
+        for i in range(3):
+            j = -1-i
+            self.assertEqual(qBlocks[j][0], projectableBlocks[i][0], "Incorrect projectable start position")
+            self.assertEqual(qBlocks[j][1], projectableBlocks[i][1], "Incorrect projectable end position")
+            self.assertEqual(rBlocks[j][0], projectionBlocks[i][0], "Incorrect projection start position")
+            self.assertEqual(rBlocks[j][1], projectionBlocks[i][1], "Incorrect projection end position")
+            self.assertEqual(cigarListTruth[j], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
+
+    def testNegativeRef2AsmIncludeEndingAndPostIndel(self):
+        alignment = self.alignmentNegative
+        mode = "ref2asm"
+        includeEndingIndel = True
+        includePostIndel = True
+        # block start and end are 1-based
+        blocks = [(14, 20, "NA"), (39, 48, "NA"), (49, 58, "NA")]
+        projectionBlocks = [(241, 247), (211, 225), (201, 210)]
+        projectableBlocks = [(14, 20), (39, 48) , (49, 58)]
+        cigarListTruth = ["1=1X2I2=1X2D" , "5=1X4=5I", "3=1X6="]
+        qBlocks, rBlocks, cigarList = findProjections(mode,
+                                                      alignment.cigarList,
+                                                      blocks,
+                                                      alignment.chromLength,
+                                                      alignment.chromStart + 1, alignment.chromEnd, # make 1-based start
+                                                      alignment.contigLength,
+                                                      alignment.contigStart + 1, alignment.contigEnd, # make 1-based start
+                                                      alignment.orientation,
+                                                      includeEndingIndel, includePostIndel)
+        for i in range(2):
+            j = -1-i
+            self.assertEqual(qBlocks[j][0], projectableBlocks[i][0], "Incorrect projectable start position")
+            self.assertEqual(qBlocks[j][1], projectableBlocks[i][1], "Incorrect projectable end position")
+            self.assertEqual(rBlocks[j][0], projectionBlocks[i][0], "Incorrect projection start position")
+            self.assertEqual(rBlocks[j][1], projectionBlocks[i][1], "Incorrect projection end position")
+            self.assertEqual(cigarListTruth[j], makeCigarString(cigarList[i]), "Incorrect CIGAR string")
+
+
+def main():
+    unittest.main(verbosity=2)
+
+if __name__ == '__main__':
+    main()
