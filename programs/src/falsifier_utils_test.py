ca61600c943d081f438e3d22306fd97a82210eed
Mobin Asri <masri@ucsc.edu>
2023-08-12 13:05:39 -0700

    Added testInducingSwitchErrorWithAnnotations

--- programs/src/falsifier_utils_test.py
+++ programs/src/falsifier_utils_test.py
@@ -147,6 +147,102 @@ class TestProjection(unittest.TestCase):
                 else:
                     self.assertListEqual(truthRelation.alignment.cigarList, outputRelation.alignment.cigarList)
 
+    def testInducingSwitchErrorWithAnnotations(self):
+        outputRelations = HomologyRelation.createAllInclusiveRelationDictFromAlignments(self.alignments, self.contigLengths, "_f")
+
+        orderIndex = 3
+        switchStart = 3
+        switchEnd = 4
+        HomologyRelation.induceSwitchErrorAndUpdateRelationsInNewContig(outputRelations, "ctg1_f", orderIndex, switchStart, switchEnd)
+
+        # ctg1_f
+        ctg1HomologyBlock1 = HomologyBlock("ctg1", 1, 6, '+', "ctg1_f", 0)
+        ctg1HomologyBlock1.annotationBlockLists = {"annot1": BlockList([(4, 6)]),
+                                                   "annot2": BlockList([(1,3)])}
+        ctg1HomologyBlock2 = HomologyBlock("ctg1", 7, 15, '+', "ctg1_f", 1)
+        ctg1HomologyBlock2.annotationBlockLists = {"annot1": BlockList([(1, 4), (8, 9)]),
+                                                   "annot2": BlockList([(5, 7)])}
+        ctg1HomologyBlock3 = HomologyBlock("ctg1", 16, 20, '+', "ctg1_f", 2)
+        ctg1HomologyBlock3.annotationBlockLists = {"annot1": BlockList([(1, 3)]),
+                                                   "annot2": BlockList([(4, 5)])}
+        ctg1HomologyBlock4 = HomologyBlock("ctg1", 21, 22, '+', "ctg1_f", 3)
+        ctg1HomologyBlock4.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1, 2)])}
+        ctg1HomologyBlock5 = HomologyBlock("ctg2", 25, 25, '-', "ctg1_f", 4)
+        ctg1HomologyBlock4.annotationBlockLists = {"annot1": BlockList([1,1]),
+                                                   "annot2": BlockList([])}
+        ctg1HomologyBlock6 = HomologyBlock("ctg1", 25, 27, '+', "ctg1_f", 5)
+        ctg1HomologyBlock6.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1, 3)])}
+        ctg1HomologyBlock7 = HomologyBlock("ctg1", 28, 30, '+', "ctg1_f", 6)
+        ctg1HomologyBlock5.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1, 3)])}
+
+        # ctg2_f
+        ctg2HomologyBlock1 = HomologyBlock("ctg2", 1, 8, '+', "ctg2_f", 0)
+        ctg2HomologyBlock1.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1,8)])}
+        ctg2HomologyBlock2 = HomologyBlock("ctg2", 9, 18, '+', "ctg2_f", 1)
+        ctg2HomologyBlock2.annotationBlockLists = {"annot1": BlockList([(1, 4)]),
+                                                   "annot2": BlockList([(5, 10)])}
+        ctg2HomologyBlock3 = HomologyBlock("ctg2", 19, 21, '+', "ctg2_f", 2)
+        ctg2HomologyBlock3.annotationBlockLists = {"annot1": BlockList([(3, 3)]),
+                                                   "annot2": BlockList([(1, 2)])}
+        ctg2HomologyBlock4 = HomologyBlock("ctg2", 22, 24, '+', "ctg2_f", 3)
+        ctg2HomologyBlock4.annotationBlockLists = {"annot1": BlockList([(1, 6)]),
+                                                   "annot2": BlockList([])}
+        ctg2HomologyBlock5 = HomologyBlock("ctg1", 23, 24, '-', "ctg2_f", 4)
+        ctg2HomologyBlock5.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1,2)])}
+        ctg2HomologyBlock6 = HomologyBlock("ctg2", 26, 27, '+', "ctg2_f", 5)
+        ctg2HomologyBlock6.annotationBlockLists = {"annot1": BlockList([(1, 2)]),
+                                                   "annot2": BlockList([])}
+        ctg2HomologyBlock7 = HomologyBlock("ctg2", 28, 30, '+', "ctg2_f", 6)
+        ctg2HomologyBlock7.annotationBlockLists = {"annot1": BlockList([]),
+                                                   "annot2": BlockList([(1,3)])}
+        ctg3HomologyBlock1 = HomologyBlock("ctg3", 1, 10, '+', "ctg3_f", 0)
+        ctg3HomologyBlock1.annotationBlockLists = {"annot1": BlockList([1,10]),
+                                                   "annot2": BlockList([])}
+
+        truthRelations = defaultdict(list)
+        truthRelations["ctg1_f"] = [HomologyRelation(ctg1HomologyBlock1, None, None, None),
+                                    HomologyRelation(ctg1HomologyBlock2, ctg2HomologyBlock2, getCigarList("1=1X2I3=1D3="), '+'),
+                                    HomologyRelation(ctg1HomologyBlock3, None, None, None),
+                                    HomologyRelation(ctg1HomologyBlock4, ctg2HomologyBlock6, getCigarList("2="), '-'),
+                                    HomologyRelation(ctg1HomologyBlock5, ctg2HomologyBlock5, getCigarList("1=1I"), '-'),
+                                    HomologyRelation(ctg1HomologyBlock6, ctg2HomologyBlock4, getCigarList("1D1X1I1="), '-'),
+                                    HomologyRelation(ctg1HomologyBlock7, None, None, None)]
+
+        truthRelations["ctg2_f"] = [HomologyRelation(ctg2HomologyBlock1, None, None, None),
+                                    HomologyRelation(ctg2HomologyBlock2, ctg1HomologyBlock2, None, None),
+                                    HomologyRelation(ctg2HomologyBlock3, None, None, None),
+                                    HomologyRelation(ctg2HomologyBlock4, ctg1HomologyBlock6, None, None),
+                                    HomologyRelation(ctg2HomologyBlock5, ctg1HomologyBlock5, None, None),
+                                    HomologyRelation(ctg2HomologyBlock6, ctg1HomologyBlock4, None, None),
+                                    HomologyRelation(ctg2HomologyBlock7, None, None, None)]
+
+        truthRelations["ctg3_f"]= [HomologyRelation(ctg3HomologyBlock1, None, None, None)]
+
+        for ctgName in ["ctg1_f", "ctg2_f", "ctg3_f"]:
+
+            self.assertTrue(ctgName in outputRelations, "Contig does not exist")
+            self.assertEqual(len(truthRelations[ctgName]), len(outputRelations[ctgName]), "Number of relations do not match")
+
+            for i in range(len(truthRelations[ctgName])):
+                truthRelation = truthRelations[ctgName][i]
+                outputRelation = outputRelations[ctgName][i]
+
+                for truthBlock, outputBlock, name in zip([truthRelation.block, truthRelation.homologousBlock], [outputRelation.block, outputRelation.homologousBlock], ["block", "homologous block"] ):
+                    if truthBlock == None:
+                        self.assertTrue(outputBlock == None, f"the block is not None ({name})")
+                    else:
+                        for name, blockList in truthBlock.annotationBlockLists.items():
+                            self.assertTrue(blockList.isEqual(outputBlock.annotationBlockLists[name]), f"annotation list is not correct ({name})")
+
+                else:
+                    self.assertListEqual(truthRelation.alignment.cigarList, outputRelation.alignment.cigarList)
+
+
     def testFillingAnnotationBlockListsFromOriginalContigs(self):
         outputRelations = HomologyRelation.createAllInclusiveRelationDictFromAlignments(self.alignments,
                                                                                       self.contigLengths,
@@ -155,14 +251,6 @@ class TestProjection(unittest.TestCase):
                                                                      self.annotationBlockListsPerOrigContig,
                                                                      self.contigLengths,
                                                                      "_f")
-
-        self.annotationBlockListsPerOrigContig = {"ctg1":{"annot1": BlockList([(4, 10),(14, 18)]),
-                                                          "annot2": BlockList([(1, 3), (11, 13), (19, 30)])},
-                                                  "ctg2":{"annot1": BlockList([(9, 12), (21, 27)]),
-                                                          "annot2": BlockList([(1,8), (13, 20), (28, 30)])},
-                                                  "ctg3":{"annot1": BlockList([(1,10)]),
-                                                          "annot2": BlockList()}
-                                                  }
 
         ctg1HomologyBlock1 = HomologyBlock("ctg1", 1, 6, '+', "ctg1_f", 0)
         ctg1HomologyBlock1.annotationBlockLists = {"annot1": BlockList([(4, 6)]),
