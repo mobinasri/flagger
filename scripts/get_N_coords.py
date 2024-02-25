343677637fe413405f1d5548defb1faa79d85b26
mobinasri <masri@ucsc.edu>
2024-02-24 19:46:08 -0800

    Revived some old python scripts

---
+++ scripts/get_N_coords.py
@@ -0,0 +1,29 @@
+import sys,re
+import argparse
+
+
+def print_coords(seq_name, seq):
+    for match in re.finditer(r'[nN]+', seq):
+        print(f'{seq_name}\t{match.start() + 1}\t{match.end()}')
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputFasta')
+    args = parser.parse_args()
+
+    seq_name = None
+    with open(args.inputFasta, "r") as f:
+        for line in f:
+            if line.strip()[0] == ">":
+                if seq_name != None:
+                    print_coords(seq_name, seq)
+                seq_name = line.strip()[1:]
+                seq = ""
+                continue
+            seq += line.strip()
+    print_coords(seq_name, seq)
+
+if __name__ == "__main__":
+    main()
