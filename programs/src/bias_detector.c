16ccd051ae3418702b698cf9d47391083cf45275
mobinasri <masri@ucsc.edu>
2024-02-29 01:02:24 -0800

    Added an option to report only annotations with more than 2Mb bases as biased

--- programs/src/bias_detector.c
+++ programs/src/bias_detector.c
@@ -66,9 +66,10 @@ int main(int argc, char *argv[]) {
     char *baseline_annot_name;
     double cov_diff_normalized_threshold = 0.15;
     int lowest_coverage = 5;
+    int min_count = 2000000; // 2Mb
     char *program;
     (program = strrchr(argv[0], '/')) ? ++program : (program = argv[0]);
-    while (~(c = getopt(argc, argv, "i:t:j:b:d:e:h"))) {
+    while (~(c = getopt(argc, argv, "i:t:j:b:d:e:m:h"))) {
         switch (c) {
             case 'i':
                 bam_path = optarg;
@@ -88,6 +89,9 @@ int main(int argc, char *argv[]) {
             case 'e':
 		lowest_coverage = atoi(optarg);
 		break;
+	    case 'm':
+		min_count = atoi(optarg);
+		break;
             default:
                 if (c != 'h') fprintf(stderr, "[E::%s] undefined option %c\n", __func__, c);
             help:
@@ -99,6 +103,7 @@ int main(int argc, char *argv[]) {
                 fprintf(stderr, "         -b         name of the baseline annotation\n");
 		fprintf(stderr, "         -d         threshold for reporting an annotation as biased or not  (It is being applied on the coverage deviation normalized by the baseline coverage) [default:0.15]\n");
 		fprintf(stderr, "         -e         the most frequent coverage will selected from among the coverages greater than or equal to the value of this parameter [default:5]\n");
+		fprintf(stderr, "         -m         min length of an annotation that can be reported as biased [default:2000000]\n");
                 return 1;
         }
     }
@@ -132,7 +137,7 @@ int main(int argc, char *argv[]) {
 	int maxCount = maxCounts[annotIndex];
         fprintf(stdout, "%s\t%s\t%d\t%+.3f\t%s\n",
                 annotationName,
-		(cov_diff_normalized_threshold < coverageDiffNormalizedAbs) && (maxCount > 10000) ? "biased" : "not_biased",
+		(cov_diff_normalized_threshold < coverageDiffNormalizedAbs) && (maxCount > min_count) ? "biased" : "not_biased",
                 mostFrequentCoverages[annotIndex],
                 coverageDiffNormalized,
 		annotationPath);
