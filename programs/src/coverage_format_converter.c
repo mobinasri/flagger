ee613e14c23622f8f92d3879f9ebdcd2a9070e19
Mobin Asri <masri@ucsc.edu>
2024-05-11 15:26:26 -0700

    indentation

--- programs/src/coverage_format_converter.c
+++ programs/src/coverage_format_converter.c
@@ -35,52 +35,57 @@ int main(int argc, char *argv[]) {
             case 'o':
                 outputPath = optarg;
                 break;
-	    case 'f':
+            case 'f':
                 faiPath = optarg;
                 break;
-	    case 'w':
-		windowLen = atoi(optarg);
-		break;
-	    case 't':
-		nThreads = atoi(optarg);
-		break;
-	    case 'n':
-		trackName = optarg;
-		break;
-	    default:
+            case 'w':
+                windowLen = atoi(optarg);
+                break;
+            case 't':
+                nThreads = atoi(optarg);
+                break;
+            case 'n':
+                trackName = optarg;
+                break;
+            default:
                 if (c != 'h') fprintf(stderr, "[E::%s] undefined option %c\n", __func__, c);
             help:
                 fprintf(stderr, "\nUsage: %s  -i <INPUT_FILE> -f <FAI> -o <OUTPUT_FILE> \n", program);
                 fprintf(stderr, "Options:\n");
-		fprintf(stderr, "         -i         input path (can have formats '.cov', '.cov.gz', '.bed' or '.bed.gz')\n");
-		fprintf(stderr, "         -f         fai path\n");
-                fprintf(stderr, "         -o         output path (can have formats '.cov', '.cov.gz', '.bed', '.bed.gz', or 'bedgraph')\n");
-		fprintf(stderr, "         -w         window length (only be used for generating wig file) [Default = 1000]\n");
-		fprintf(stderr, "         -t         number of threads for taking window average (only be used for generating wig file) [Default = 4]\n");
-		fprintf(stderr, "         -n         track name (only be used for generating wig file) [Default = 'coverage_wig']\n");
+                fprintf(stderr,
+                        "         -i         input path (can have formats '.cov', '.cov.gz', '.bed' or '.bed.gz')\n");
+                fprintf(stderr, "         -f         fai path\n");
+                fprintf(stderr,
+                        "         -o         output path (can have formats '.cov', '.cov.gz', '.bed', '.bed.gz', or 'bedgraph')\n");
+                fprintf(stderr,
+                        "         -w         window length (only be used for generating wig file) [Default = 1000]\n");
+                fprintf(stderr,
+                        "         -t         number of threads for taking window average (only be used for generating wig file) [Default = 4]\n");
+                fprintf(stderr,
+                        "         -n         track name (only be used for generating wig file) [Default = 'coverage_wig']\n");
                 return 1;
         }
     }
     char *inputExtension = extractFileExtension(inputPath);
     char *outputExtension = extractFileExtension(outputPath);
     if (strcmp(outputExtension, "cov") != 0 &&
-	strcmp(outputExtension, "cov.gz") != 0 &&
-	strcmp(outputExtension, "bed") != 0 &&
-	strcmp(outputExtension, "bed.gz") != 0 &&
-	strcmp(outputExtension, "bedgraph") != 0){
-	    fprintf(stderr, "[%s] Error: output file should either cov/cov.gz/bed/bed.gz/bedgraph  !\n", get_timestamp());
-	    free(inputExtension);
-	    free(outputExtension);
-	    exit(EXIT_FAILURE);
+        strcmp(outputExtension, "cov.gz") != 0 &&
+        strcmp(outputExtension, "bed") != 0 &&
+        strcmp(outputExtension, "bed.gz") != 0 &&
+        strcmp(outputExtension, "bedgraph") != 0) {
+        fprintf(stderr, "[%s] Error: output file should either cov/cov.gz/bed/bed.gz/bedgraph  !\n", get_timestamp());
+        free(inputExtension);
+        free(outputExtension);
+        exit(EXIT_FAILURE);
     }
     if (strcmp(inputExtension, "cov") != 0 &&
         strcmp(inputExtension, "cov.gz") != 0 &&
         strcmp(inputExtension, "bed") != 0 &&
-        strcmp(inputExtension, "bed.gz") != 0){
-            fprintf(stderr, "[%s] Error: input file should either cov/cov.gz/bed/bed.gz  !\n", get_timestamp());
-	    free(inputExtension);
-            free(outputExtension);
-            exit(EXIT_FAILURE);
+        strcmp(inputExtension, "bed.gz") != 0) {
+        fprintf(stderr, "[%s] Error: input file should either cov/cov.gz/bed/bed.gz  !\n", get_timestamp());
+        free(inputExtension);
+        free(outputExtension);
+        exit(EXIT_FAILURE);
     }
 
     fprintf(stderr, "[%s] Parsing %s.\n", get_timestamp(), faiPath);
@@ -88,60 +93,64 @@ int main(int argc, char *argv[]) {
 
     char trackName2[100];
     char color[100];
-    if (strcmp(outputExtension, "bedgraph") == 0){
-	    char outputPath2[1000];
-	    char prefix[1000];
-	    memcpy(prefix, outputPath, strlen(outputPath)-9);
-	    prefix[strlen(outputPath)-9] = '\0';
+    if (strcmp(outputExtension, "bedgraph") == 0) {
+        char outputPath2[1000];
+        char prefix[1000];
+        memcpy(prefix, outputPath, strlen(outputPath) - 9);
+        prefix[strlen(outputPath) - 9] = '\0';
 
-            int chunkCanonicalLen = ptBlock_get_max_contig_length(ctg_to_len);
-            ChunksCreator *chunksCreator = ChunksCreator_constructFromCov(inputPath, faiPath, chunkCanonicalLen, nThreads, windowLen);
-	    ChunksCreator_parseChunks(chunksCreator);
-	    ChunksCreator_sortChunks(chunksCreator);
+        int chunkCanonicalLen = ptBlock_get_max_contig_length(ctg_to_len);
+        ChunksCreator *chunksCreator = ChunksCreator_constructFromCov(inputPath, faiPath, chunkCanonicalLen, nThreads,
+                                                                      windowLen);
+        ChunksCreator_parseChunks(chunksCreator);
+        ChunksCreator_sortChunks(chunksCreator);
 
-	    fprintf(stderr, "[%s] Writing %s.\n", get_timestamp(), outputPath);
-	    // since chunkCanonicalLen is set to maximum contig size
-	    // then each contig is parsed in a single chunk
-	    sprintf(trackName2, "%s_total", trackName);
-	    sprintf(outputPath2, "%s.bedgraph", prefix);
-	    ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverage, "w", "51,51,255");
-	    
-	    sprintf(trackName2, "%s_high_mapq", trackName);
-	    sprintf(outputPath2, "%s.high_mapq.bedgraph", prefix);
-            ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverageHighMapq, "w", "0,0,153");
-	    
-	    sprintf(trackName2, "%s_high_clip", trackName);
-	    sprintf(outputPath2, "%s.high_clip.bedgraph", prefix);
-            ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverageHighClip, "w", "204,0,204");
+        fprintf(stderr, "[%s] Writing %s.\n", get_timestamp(), outputPath);
+        // since chunkCanonicalLen is set to maximum contig size
+        // then each contig is parsed in a single chunk
+        sprintf(trackName2, "%s_total", trackName);
+        sprintf(outputPath2, "%s.bedgraph", prefix);
+        ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverage, "w",
+                                              "51,51,255");
 
-	    sprintf(trackName2, "%s_region_idx", trackName);
-	    sprintf(outputPath2, "%s.region_idx.bedgraph", prefix);
-            ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getRegionIndex, "w", "64,64,64");
-	    
-	    ChunksCreator_destruct(chunksCreator);
-	    stHash_destruct(ctg_to_len);
-	    free(outputExtension);
-	    fprintf(stderr, "[%s] Done!\n", get_timestamp());
-	    return 0;
+        sprintf(trackName2, "%s_high_mapq", trackName);
+        sprintf(outputPath2, "%s.high_mapq.bedgraph", prefix);
+        ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverageHighMapq,
+                                              "w", "0,0,153");
+
+        sprintf(trackName2, "%s_high_clip", trackName);
+        sprintf(outputPath2, "%s.high_clip.bedgraph", prefix);
+        ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getCoverageHighClip,
+                                              "w", "204,0,204");
+
+        sprintf(trackName2, "%s_region_idx", trackName);
+        sprintf(outputPath2, "%s.region_idx.bedgraph", prefix);
+        ChunksCreator_writeChunksIntoBedGraph(chunksCreator, outputPath2, trackName2, CoverageInfo_getRegionIndex, "w",
+                                              "64,64,64");
+
+        ChunksCreator_destruct(chunksCreator);
+        stHash_destruct(ctg_to_len);
+        free(outputExtension);
+        fprintf(stderr, "[%s] Done!\n", get_timestamp());
+        return 0;
     }
 
     fprintf(stderr, "[%s] Parsing %s.\n", get_timestamp(), inputPath);
     stHash *coverage_blocks_per_contig = ptBlock_parse_coverage_info_blocks(inputPath);
 
     fprintf(stderr, "[%s] Writing %s.\n", get_timestamp(), outputPath);
-    if(strcmp(outputExtension, "cov") == 0){
-	FILE* fp = fopen(outputPath, "w");
+    if (strcmp(outputExtension, "cov") == 0) {
+        FILE *fp = fopen(outputPath, "w");
         if (fp == NULL) {
             fprintf(stderr, "[%s] Error: Failed to open file %s.\n", get_timestamp(), outputPath);
         }
-	ptBlock_print_blocks_stHash_in_cov(coverage_blocks_per_contig,
+        ptBlock_print_blocks_stHash_in_cov(coverage_blocks_per_contig,
                                            get_string_cov_info_data_format_2,
                                            fp,
                                            false,
                                            ctg_to_len);
-	fclose(fp);
-    }
-    else if(strcmp(outputExtension, "cov.gz") == 0){
+        fclose(fp);
+    } else if (strcmp(outputExtension, "cov.gz") == 0) {
         gzFile fp = gzopen(outputPath, "w6h");
         if (fp == Z_NULL) {
             fprintf(stderr, "[%s] Error: Failed to open file %s.\n", get_timestamp(), outputPath);
@@ -151,11 +160,10 @@ int main(int argc, char *argv[]) {
                                            &fp,
                                            true,
                                            ctg_to_len);
-	gzclose(fp);
+        gzclose(fp);
 
-    }
-    else if(strcmp(outputExtension, "bed") == 0){
-        FILE* fp = fopen(outputPath, "w");
+    } else if (strcmp(outputExtension, "bed") == 0) {
+        FILE *fp = fopen(outputPath, "w");
         if (fp == NULL) {
             fprintf(stderr, "[%s] Error: Failed to open file %s.\n", get_timestamp(), outputPath);
         }
@@ -164,8 +172,7 @@ int main(int argc, char *argv[]) {
                                            fp,
                                            false);
         fclose(fp);
-    }
-    else if(strcmp(outputExtension, "bed.gz") == 0){
+    } else if (strcmp(outputExtension, "bed.gz") == 0) {
         gzFile fp = gzopen(outputPath, "w6h");
         if (fp == Z_NULL) {
             fprintf(stderr, "[%s] Error: Failed to open file %s.\n", get_timestamp(), outputPath);
