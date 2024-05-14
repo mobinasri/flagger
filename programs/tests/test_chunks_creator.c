1b21789680c6b9db3d0d40766feef78bc88b2792
Mobin Asri <masri@ucsc.edu>
2024-05-14 11:26:05 -0700

    Added a test for creating chunks with labels

--- programs/tests/test_chunks_creator.c
+++ programs/tests/test_chunks_creator.c
@@ -17,15 +17,15 @@ bool testCreatingChunks(char *covPath) {
                                  {100, 109, 16, 16, 16, 1}};
     // annotation indices
     int truthValuesCtg2[1][6] = {{0, 9, 7, 7, 1, 0}};
-    int truthAnnotationsCtg1[6][2] = {{1,-1},
-                                      {1,-1},
-                                      {1,-1},
-                                      {1,2},
-                                      {2,-1},
-                                      {2,-1}};
-    int truthAnnotationsCtg2[1][2] = {{1,2}};
+    int truthAnnotationsCtg1[6][2] = {{1, -1},
+                                      {1, -1},
+                                      {1, -1},
+                                      {1, 2},
+                                      {2, -1},
+                                      {2, -1}};
+    int truthAnnotationsCtg2[1][2] = {{1, 2}};
 
-    int truthAnnotationsLenCtg1[6] = {1,1,1,2,1,1};
+    int truthAnnotationsLenCtg1[6] = {1, 1, 1, 2, 1, 1};
     int truthAnnotationsLenCtg2[1] = {2};
 
     int windowLen = 20;
@@ -48,7 +48,7 @@ bool testCreatingChunks(char *covPath) {
                 CoverageInfo *coverageInfo = chunk->coverageInfoSeq[windowIndex];
                 int *truthValues = truthValuesCtg1[trackIndexCtg1];
                 int *truthAnnotations = truthAnnotationsCtg1[trackIndexCtg1];
-		int truthAnnnotationsLen = truthAnnotationsLenCtg1[trackIndexCtg1];
+                int truthAnnnotationsLen = truthAnnotationsLenCtg1[trackIndexCtg1];
                 int s = chunk->s + windowLen * windowIndex;
                 int e = min(chunk->e, s + windowLen - 1);
                 correct &= (s == truthValues[0]);
@@ -60,7 +60,7 @@ bool testCreatingChunks(char *covPath) {
                 int len;
                 int *annotationIndices = CoverageInfo_getAnnotationIndices(coverageInfo, &len);
                 if (len != truthAnnnotationsLen) return false;
-                for(int i=0; i < len; i++){
+                for (int i = 0; i < len; i++) {
                     correct &= (annotationIndices[i] == truthAnnotations[i]);
                 }
                 free(annotationIndices);
@@ -72,7 +72,7 @@ bool testCreatingChunks(char *covPath) {
                 CoverageInfo *coverageInfo = chunk->coverageInfoSeq[windowIndex];
                 int *truthValues = truthValuesCtg2[trackIndexCtg2];
                 int *truthAnnotations = truthAnnotationsCtg2[trackIndexCtg2];
-		int truthAnnnotationsLen = truthAnnotationsLenCtg2[trackIndexCtg2];
+                int truthAnnnotationsLen = truthAnnotationsLenCtg2[trackIndexCtg2];
                 int s = chunk->s + windowLen * windowIndex;
                 int e = min(chunk->e, s + windowLen - 1);
                 correct &= (s == truthValues[0]);
@@ -83,10 +83,134 @@ bool testCreatingChunks(char *covPath) {
                 correct &= (CoverageInfo_getRegionIndex(coverageInfo) == truthValues[5]);
                 int len;
                 int *annotationIndices = CoverageInfo_getAnnotationIndices(coverageInfo, &len);
-		if (len != truthAnnnotationsLen) return false;
-                for(int i=0; i < len; i++){
+                if (len != truthAnnnotationsLen) return false;
+                for (int i = 0; i < len; i++) {
                     correct &= (annotationIndices[i] == truthAnnotations[i]);
                 }
+                free(annotationIndices);
+            }
+        }
+    }
+    ChunksCreator_destruct(chunksCreator);
+    return correct;
+}
+
+
+bool testCreatingChunksWithLabels(char *covPath) {
+    bool correct = true;
+    // start (0-based), end (0-based), coverage, high_mapq, high_clip, region_index
+    int truthValuesCtg1[6][6] = {{0,   19,  5,  2,  2,  0},
+                                 {20,  39,  10, 10, 10, 1},
+                                 {40,  59,  10, 10, 10, 1},
+                                 {60,  79,  15, 15, 13, 1},
+                                 {80,  99,  16, 16, 16, 1},
+                                 {100, 109, 16, 16, 16, 1}};
+    // annotation indices
+    int truthValuesCtg2[1][6] = {{0, 9, 7, 7, 1, 0}};
+    int truthAnnotationsCtg1[6][2] = {{1, -1},
+                                      {1, -1},
+                                      {1, -1},
+                                      {1, 2},
+                                      {2, -1},
+                                      {2, -1}};
+    int truthAnnotationsCtg2[1][2] = {{1, 2}};
+
+    int truthAnnotationsLenCtg1[6] = {1,
+                                      1,
+                                      1,
+                                      2,
+                                      1,
+                                      1};
+    int truthAnnotationsLenCtg2[1] = {2};
+
+    int truthLabelsCtg1[6] = {{1, 1},
+                              {2, 3},
+                              {2, 3},
+                              {3, 3},
+                              {3, 3},
+                              {3, 3}};
+
+    int truthLabelsCtg2[1] = {{2, 2}};
+
+    int windowLen = 20;
+    int chunkCanonicalLen = 40;
+    int nThreads = 2;
+    int trackIndexCtg1 = -1;
+    int trackIndexCtg2 = -1;
+    ChunksCreator *chunksCreator = ChunksCreator_constructFromCov(covPath, NULL, chunkCanonicalLen, nThreads,
+                                                                  windowLen);
+    if (ChunksCreator_parseChunks(chunksCreator) != 0) {
+        return false;
+    }
+    stList *chunks = chunksCreator->chunks;
+    int numberOfChunks = stList_length(chunks);
+    for (int chunkIndex = 0; chunkIndex < numberOfChunks; chunkIndex++) {
+        Chunk *chunk = stList_get(chunks, chunkIndex);
+        if (strcmp(chunk->ctg, "ctg1") == 0) {
+            for (int windowIndex = 0; windowIndex < chunk->coverageInfoSeqLen; windowIndex++) {
+                trackIndexCtg1 += 1;
+                CoverageInfo *coverageInfo = chunk->coverageInfoSeq[windowIndex];
+                int *truthValues = truthValuesCtg1[trackIndexCtg1];
+                int s = chunk->s + windowLen * windowIndex;
+                int e = min(chunk->e, s + windowLen - 1);
+                correct &= (s == truthValues[0]);
+                correct &= (e == truthValues[1]);
+                correct &= (coverageInfo->coverage == truthValues[2]);
+                correct &= (coverageInfo->coverage_high_mapq == truthValues[3]);
+                correct &= (coverageInfo->coverage_high_clip == truthValues[4]);
+                correct &= (CoverageInfo_getRegionIndex(coverageInfo) == truthValues[5]);
+
+                // check annotations
+                int *truthAnnotations = truthAnnotationsCtg1[trackIndexCtg1];
+                int truthAnnnotationsLen = truthAnnotationsLenCtg1[trackIndexCtg1];
+                int len;
+                int *annotationIndices = CoverageInfo_getAnnotationIndices(coverageInfo, &len);
+                if (len != truthAnnnotationsLen) return false;
+                for (int i = 0; i < len; i++) {
+                    correct &= (annotationIndices[i] == truthAnnotations[i]);
+                }
+
+                // check labels
+                int *labels = truthLabelsCtg1[trackIndexCtg1];
+                if (coverageInfo->data == NULL) return false;
+                Inference *inference = coverageInfo->data;
+                correct &= (inference->truth == labels[0]);
+                correct &= (inference->prediction == labels[1]);
+                free(annotationIndices);
+            }
+        }
+        if (strcmp(chunk->ctg, "ctg2") == 0) {
+            for (int windowIndex = 0; windowIndex < chunk->coverageInfoSeqLen; windowIndex++) {
+                trackIndexCtg2 += 1;
+                CoverageInfo *coverageInfo = chunk->coverageInfoSeq[windowIndex];
+                int *truthValues = truthValuesCtg2[trackIndexCtg2];
+                int s = chunk->s + windowLen * windowIndex;
+                int e = min(chunk->e, s + windowLen - 1);
+                correct &= (s == truthValues[0]);
+                correct &= (e == truthValues[1]);
+                correct &= (coverageInfo->coverage == truthValues[2]);
+                correct &= (coverageInfo->coverage_high_mapq == truthValues[3]);
+                correct &= (coverageInfo->coverage_high_clip == truthValues[4]);
+                correct &= (CoverageInfo_getRegionIndex(coverageInfo) == truthValues[5]);
+
+
+                // check annotations
+                int *truthAnnotations = truthAnnotationsCtg2[trackIndexCtg2];
+                int truthAnnnotationsLen = truthAnnotationsLenCtg2[trackIndexCtg2];
+                int len;
+                int *annotationIndices = CoverageInfo_getAnnotationIndices(coverageInfo, &len);
+                if (len != truthAnnnotationsLen) return false;
+                for (int i = 0; i < len; i++) {
+                    correct &= (annotationIndices[i] == truthAnnotations[i]);
+                }
+                free(annotationIndices);
+
+                // check labels
+                int *labels = truthLabelsCtg2[trackIndexCtg2];
+                if (coverageInfo->data == NULL) return false;
+                Inference *inference = coverageInfo->data;
+                correct &= (inference->truth == labels[0]);
+                correct &= (inference->prediction == labels[1]);
                 free(annotationIndices);
             }
         }
@@ -111,6 +235,12 @@ int main(int argc, char *argv[]) {
     printf("[chunks_creator] Test creating chunks from compressed file:");
     printf(test2Passed ? "\x1B[32m OK \x1B[0m\n" : "\x1B[31m FAIL \x1B[0m\n");
     allTestsPassed &= test2Passed;
+
+    // test 3
+    bool test3Passed = testCreatingChunksWithLabels("tests/test_files/chunks_creator/test_1_with_labels.cov");
+    printf("[chunks_creator] Test creating chunks from uncompressed file with labels:");
+    printf(test3Passed ? "\x1B[32m OK \x1B[0m\n" : "\x1B[31m FAIL \x1B[0m\n");
+    allTestsPassed &= test3Passed;
 
     if (allTestsPassed)
         return 0;
