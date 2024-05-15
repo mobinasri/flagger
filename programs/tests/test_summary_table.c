cb077679a521e4ab32e92fbe0638a0759a39024e
mobinasri <masri@ucsc.edu>
2024-05-15 11:51:12 -0700

    Added test for string functions

--- programs/tests/test_summary_table.c
+++ programs/tests/test_summary_table.c
@@ -42,6 +42,33 @@ bool test_SummaryTable_increment() {
     return correct;
 }
 
+bool test_SummaryTable_getRowString() {
+    int numberOfRows = 2;
+    int numberOfColumns = 2;
+    SummaryTable *summaryTable = SummaryTable_construct(numberOfRows, numberOfColumns);
+
+    SummaryTable_increment(summaryTable, 0, 0, 10);
+    SummaryTable_increment(summaryTable, 0, 0, 10);
+    SummaryTable_increment(summaryTable, 0, 0, 10);
+    SummaryTable_increment(summaryTable, 0, 1, 10);
+    SummaryTable_increment(summaryTable, 0, 1, 10);
+
+    bool correct = true;
+
+    const char *rowOneString = "30.00,20.00";
+    const char *rowTwoString = "0.00,0.00";
+    const char *rowOneStringPercentage = "60.00,40.00";
+    const char *rowTwoStringPercentage = "0.00,0.00";
+
+    correct &= strcmp(SummaryTable_getRowString(summaryTable, 0, ','), rowOneString) == 0;
+    correct &= strcmp(SummaryTable_getRowString(summaryTable, 1, ','), rowTwoString) == 0;
+    correct &= strcmp(SummaryTable_getRowStringPercentage(summaryTable, 0, ','), rowOneStringPercentage) == 0;
+    correct &= strcmp(SummaryTable_getRowStringPercentage(summaryTable, 1, ','), rowTwoStringPercentage) == 0;
+
+    SummaryTable_destruct(summaryTable);
+    return correct;
+}
+
 
 int main(int argc, char *argv[]) {
 
@@ -52,6 +79,12 @@ int main(int argc, char *argv[]) {
     printf("[summary_table] Test test_SummaryTable_increment:");
     printf(test1Passed ? "\x1B[32m OK \x1B[0m\n" : "\x1B[31m FAIL \x1B[0m\n");
     allTestsPassed &= test1Passed;
+
+    // test 2
+    bool test2Passed = test_SummaryTable_getRowString();
+    printf("[summary_table] Test test_SummaryTable_getRowString:");
+    printf(test2Passed ? "\x1B[32m OK \x1B[0m\n" : "\x1B[31m FAIL \x1B[0m\n");
+    allTestsPassed &= test2Passed;
 
     if (allTestsPassed)
         return 0;
