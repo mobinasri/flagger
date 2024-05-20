862666ea909a216d2d98f5513fc5f94d42daf076
Mobin Asri <masri@ucsc.edu>
2024-05-20 12:50:54 -0700

    destruct args

--- programs/tests/test_summary_table.c
+++ programs/tests/test_summary_table.c
@@ -431,6 +431,7 @@ int test_ptBlock_updateSummaryTableListWithIterator(const char *covPath, const c
     Double_destruct2DArray(annotation2Bin1, 4);
     Double_destruct2DArray(annotation1Bin2, 4);
     Double_destruct2DArray(annotation2Bin2, 4);
+    SummaryTableUpdaterArgs_destruct(argsTemplate);
 
 
     return correct;
